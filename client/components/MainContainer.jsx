e21d89de8c1bc46be00eaea857d245a76d6c2df1
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-04 16:48:41 -0700

    Adds functionality to add item to clothing_item table

--- client/components/MainContainer.jsx
+++ client/components/MainContainer.jsx
@@ -1,46 +1,46 @@
 // 2. MainContainer.jsx (displays category boxes)
 //     ---------------------------------------------------------------------------------
 //     Input: User id
-//     Renders: Menu buttons 
-//     Child Conponent: CategoryDisplay.jsx <Menu category = 'tops' userId = 'asefjowef83r8'> 
+//     Renders: Menu buttons
+//     Child Conponent: CategoryDisplay.jsx <Menu category = 'tops' userId = 'asefjowef83r8'>
 //                      * actual props should be variable names
 //     ---------------------------------------------------------------------------------
 
-
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 //import CategoryDisplay from './CategoryDisplay'
+import AddItem from "./AddItem";
 
 export const GlobalContext = React.createContext({}); // To pass state to children
-function MainContainer({children}){  
-const [resourceType, setResourceType] = useState(''); // State update
+function MainContainer({ children }) {
+	const [resourceType, setResourceType] = useState(""); // State update
 
+	// if(resourceType === 'accessory') {
+	//   menuType =  `WE ARE NOW LOOKING AT ACCESSORIES`;
+	// }
+	// if(resourceType === 'tops') {
+	//   menuType = `WE ARE NOW LOOKING AT TOPS ? YES, ${resourceType}`;
+	// }
+	// if(resourceType === 'bottoms') {
+	//   menuType =  `WE ARE NOW LOOKING AT BOTTOMS ? YES, ${resourceType}`;
+	// }
+	// if(resourceType === 'shoes') {
+	//   menuType = `WE ARE NOW LOOKING AT SHOES ? YES, ${resourceType}`;
+	// }
 
+	return (
+		<div>
+			<button onClick={() => setResourceType("accessories")}>Accessory</button>
+			<button onClick={() => setResourceType("tops")}>Tops</button>
+			<button onClick={() => setResourceType("bottoms")}>Bottoms</button>
+			<button onClick={() => setResourceType("shoes")}>Shoes</button>
+			<h1>{resourceType}</h1>
+			<h2>Now you can choose {resourceType}</h2>
+			{/* <CategoryDisplay category = {resourceType} userId = 'asefjowef83r8' />   */}
 
-  // if(resourceType === 'accessory') {
-  //   menuType =  `WE ARE NOW LOOKING AT ACCESSORIES`;
-  // } 
-  // if(resourceType === 'tops') {
-  //   menuType = `WE ARE NOW LOOKING AT TOPS ? YES, ${resourceType}`;
-  // }
-  // if(resourceType === 'bottoms') {
-  //   menuType =  `WE ARE NOW LOOKING AT BOTTOMS ? YES, ${resourceType}`;
-  // } 
-  // if(resourceType === 'shoes') {
-  //   menuType = `WE ARE NOW LOOKING AT SHOES ? YES, ${resourceType}`;
-  // }  
-
-
-  return(
-    <div>
-      <button onClick={() => setResourceType('accessory')}>Accessory</button>
-      <button onClick={() => setResourceType('tops')}>Tops</button>
-      <button onClick={() => setResourceType('bottoms')}>Bottoms</button>
-      <button onClick={() => setResourceType('shoes')}>Shoes</button>
-      <h1>{resourceType}</h1>
-      <h2>Now you can choose {resourceType}</h2>
-      {/* <CategoryDisplay category = {resourceType} userId = 'asefjowef83r8' />   */}
-    </div>
-  )
+			{/* Added AddItem here temporarily */}
+			<AddItem resourceType={resourceType} user_id={1} />
+		</div>
+	);
 }
 
 export default MainContainer;
